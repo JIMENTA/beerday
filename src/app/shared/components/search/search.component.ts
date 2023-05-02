8e9f33537037e21e122198b0d0ce9c10e11330a5
menta <jimementa@gmail.com>
2023-05-02 02:02:06 +0200

    create debounce and clean subscriptions

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-search',
@@ -6,15 +7,37 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+
+  private debouncer : Subject<string> = new Subject<string>()
+  private debouncerSubscription?: Subscription
+
+
   @Input()
   public placeholder  : string = '';
 
   @Output()
-  public onValue = new EventEmitter <string>();
-  
-  sendValue( value : string):void{
-    this.onValue.emit(value)
+  public onDebounce = new EventEmitter <string>();
+
+  ngOnInit(): void {
+   this.debouncerSubscription= this.debouncer
+   .pipe(
+    debounceTime(300)
+   )
+   .subscribe( value => {
+    this.onDebounce.emit(value)
+   })
   }
+
+  ngOnDestroy():void{
+    this.debouncerSubscription?.unsubscribe()
+
+  }
+
+  onKeyPress(searchTerm : string){
+    this.debouncer.next(searchTerm) 
+ }
+
+
 
 
 }
