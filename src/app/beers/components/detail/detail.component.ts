90cece8cd177029f8597953b804b76d42dfd795a
menta <jimementa@gmail.com>
2023-05-03 13:29:22 +0200

    create card detail

--- src/app/beers/components/detail/detail.component.ts
+++ src/app/beers/components/detail/detail.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Beer } from '../../interfaces/beer.interface.';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BeersService } from '../../services/beers.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-detail',
@@ -7,6 +10,25 @@ import { Beer } from '../../interfaces/beer.interface.';
   styleUrls: ['./detail.component.css']
 })
 export class DetailComponent {
-  public beers : Beer[] =[]
+  @Input()
+  public beerId ?: Beer | null;
+
+
+  constructor (private activatedRoute : ActivatedRoute, private router : Router, private beersService : BeersService ){}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+    .pipe(
+      switchMap(  ({ id }) => this.beersService.searchById(id))
+    )
+    .subscribe( beer => {
+      if(!beer)this.router.navigateByUrl('');
+
+      return this.beerId = beer
+    
+    })
+
+  }
+
 
 }
